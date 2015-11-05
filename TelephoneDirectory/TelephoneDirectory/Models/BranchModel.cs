72dc56e07b35ae4c19ec5da15418ed4dc4a2f218
Elias Girma <elias_girma@hotmail.com>
2015-11-05 15:14:45 +0300

    Created Some Models

---
+++ TelephoneDirectory/TelephoneDirectory/Models/BranchModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace TelephoneDirectory.Models
+{
+
+
+    [Table("Branch")]
+    public class BranchModel
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int BranchId { get; set; }
+        public string BranchCode { get; set; }
+        public string BranchName { get; set; }
+    }
+}
---
+++ TelephoneDirectory/TelephoneDirectory/Models/PhoneDirectoryModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TelephoneDirectory.Models
+{
+    [Table("PhoneDirectory")]
+    public class PhoneDirectoryModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public string DirectTelephone { get; set; }
+        public string MobileNo { get; set; }
+        public string ExtentionNo { get; set; }
+        
+    }
+}
